d5e4588e6dd8b965daa9bf23d6618a639d3ab547
guochao <with1the@gmail.com>
2017-05-03 15:03:19 +0800

    Finish user singup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,4 +20,5 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
-Route::get('/singup','UserController@creat')->name('singup');
+Route::get('/singup','UsersController@creat')->name('singup');
+Route::resource('users', 'UsersController');
